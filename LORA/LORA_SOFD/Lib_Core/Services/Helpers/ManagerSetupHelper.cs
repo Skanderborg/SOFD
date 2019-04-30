f25f5cf2b5bdd4f9c09440ab8903843563d13684
Jacob Hansen <jacob@digistic.dk>
2019-04-30 13:49:55 +0200

    manager lookup table in the works

---
+++ LORA/LORA_SOFD/Lib_Core/Services/Helpers/ManagerSetupHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL_old;
+using DAL_old.LORA_SOFD;
+
+namespace Lib_Core.Services.Helpers
+{
+    public class ManagerSetupHelper
+    {
+        private IRepo<Manager_lookup> mlRepo;
+        private IRepo<Position> posRepo;
+        private IRepo<Orgunit> orgRepo;
+
+        public ManagerSetupHelper(string constr)
+        {
+            mlRepo = new ManagerLookupRepo(constr);
+            posRepo = new PositionRepo(constr);
+            orgRepo = new OrgunitRepo(constr);
+        }
+
+        private void Delte_Manager_Lookups()
+        {
+            foreach(Manager_lookup ml in mlRepo.Query)
+            {
+                if (posRepo.Query.Where(p => p.Opus_id == ml.opus_id).Count() == 0)
+                {
+                    mlRepo.Delete(ml);
+                }
+            }
+        }
+
+        private void Update_Manager_Lookups()
+        {
+            foreach (Position pos in posRepo.Query.Where(p => p.Last_changed > DateTime.Now.AddDays(-3)))
+            {
+
+            }
+        }
+
+        private int GetNearestManagerOpusId()
+        {
+            
+        }
+
+    }
+}
