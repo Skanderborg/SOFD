5048b46d8f7ed3ab50435043fe9b88144da6dd68
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-08-09 08:30:23 +0200

    os2rollekatalog sync + models

---
+++ LORA/LORA_SOFD/OS2Rollekatalog/Model/OrgUnit.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace OS2Rollekatalog.Model
+{
+    internal class OrgUnit
+    {
+        [JsonProperty(PropertyName = "uuid", Required = Required.Always)]
+        internal string uuid { get; set; }
+
+        [JsonProperty(PropertyName = "name", Required = Required.Always)]
+        internal string name { get; set; }
+
+        [JsonProperty(PropertyName = "employees")]
+        internal List<Employee> employees { get; set; }
+
+        [JsonProperty(PropertyName = "kle-performing")]
+        internal List<string> kle_performing { get; set; }
+
+        [JsonProperty(PropertyName = "kle-interest")]
+        internal List<string> kle_interest { get; set; }
+
+        [JsonProperty(PropertyName = "children")]
+        internal List<OrgUnit> children { get; set; }
+    }
+}
