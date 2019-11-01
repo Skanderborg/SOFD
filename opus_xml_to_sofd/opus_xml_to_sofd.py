96066256312c3b37041079b9179f4a8a64bf0da7
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-11-01 14:13:35 +0100

    orgunit manager recursion

--- opus_xml_to_sofd/opus_xml_to_sofd.py
+++ opus_xml_to_sofd/opus_xml_to_sofd.py
@@ -49,3 +49,28 @@ except:
                  'Error: opus_xml_to_sofd python app', step)
 
 
+'''
+# henter stien til den sti hvor vores kfs-lan udtræk for OPUS medarbejder data er placeret
+xml_path = os.environ.get('employee_org_xml_path')
+# connection string til SOFD databasen
+constr_lora = os.environ.get('constr_lora')
+
+# finder frem til den seneste fil fra OPUS
+list_of_files = glob.glob(xml_path)
+latest_file = max(list_of_files, key=os.path.getctime)
+step = 'setup complete'
+
+# opdaterer orgunits fra OPUS til SOFD
+org_service = Orgunit_service(latest_file, constr_lora)
+org_service.update_orgunits()
+step = 'org units complete'
+
+# opdaterer employee fra OPUS til position og person i SOFD
+emp_service = Employee_service(latest_file, constr_lora)
+emp_service.build_people_and_positions_from_opusxml()
+step = 'build pos + per complete'
+emp_service.update_persons()
+step = 'update persons complete'
+emp_service.update_positions()
+step = 'update positions complete'
+'''
--- opus_xml_to_sofd/service/manager_setup_service.py
+++ opus_xml_to_sofd/service/manager_setup_service.py
@@ -4,13 +4,12 @@ from dal.position_repo import Position_repo
 
 class Manager_setup_service:
     def __init__(self, constr_lora):
-        self.constr_lora = constr_lora
+        self.org_repo = Orgunit_repo(constr_lora)
+        self.pos_repo = Position_repo(constr_lora)
 
     def set_orgunit_manager(self):
-        org_repo = Orgunit_repo(self.constr_lora)
-        pos_repo = Position_repo(self.constr_lora)
-        orgs = org_repo.get_orgunits()
-        managers = pos_repo.get_positions('WHERE [is_manager] = 1')
+        orgs = self.org_repo.get_orgunits()
+        managers = self.pos_repo.get_positions('WHERE [is_manager] = 1')
 
         for mkey in managers:
             manager = managers[mkey]
@@ -22,4 +21,17 @@ class Manager_setup_service:
                 org_repo.update_orgunit(org)
 
     def set_nearest_manager(self):
-        
+        org_repo = Orgunit_repo(self.constr_lora)
+        pos_repo = Position_repo(self.constr_lora)
+        orgs = org_repo.get_orgunits()
+        positions = pos_repo.get_positions()
+
+        for pkey in positions:
+            x = 1
+
+    def get_manager(self, los_id):
+        orgunit = self.org_repo.get_orgunits('WHERE [los_id] = ' + los_id)[0]
+        if orgunit.manager_opus_id != None:
+            return orgunit.manager_opus_id
+        else:
+            return get_manager(orgunit.parent_orgunit_los_id)
