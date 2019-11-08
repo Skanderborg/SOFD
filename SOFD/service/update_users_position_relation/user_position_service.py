65685dc6027fef43b95443e2dd86b57e47fc9d35
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-11-08 14:21:43 +0100

    usr/pos relation updated

---
+++ SOFD/service/update_users_position_relation/user_position_service.py
@@ -0,0 +1,8 @@
+from dal.position_repo import Position_repo
+from dal.users_repo import User_repo
+from model.user import User
+from model.position import Position
+
+class User_position_service:
+    def __init__(self, constr_lora):
+        self.constr_lora = constr_lora
