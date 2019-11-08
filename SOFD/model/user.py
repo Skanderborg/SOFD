1ec334ab8c65ec4f96bd175e9cc618424abfea6e
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-11-08 14:10:07 +0100

    preperation for users to be tied with positions

---
+++ SOFD/model/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, opus_id, uuid, samaccount, email, phone, mobile):
+        self.opus_id = opus_id
+        self.uuid = uuid
+        self.samaccount = samaccount
+        self.email = email
+        self.phone = phone
+        self.mobile = mobile
+        
