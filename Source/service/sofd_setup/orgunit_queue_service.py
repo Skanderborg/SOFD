c20c6f357d2f92b536bbe4f9fbed1920a68eeb2e
Jacob Ågård Bennike <jacob@regnekraft.dk>
2019-12-17 14:11:21 +0100

    orgunit queue updated

--- Source/service/sofd_setup/orgunit_queue_service.py
+++ Source/service/sofd_setup/orgunit_queue_service.py
@@ -2,38 +2,37 @@ from dal.orgunit_repo import Orgunit_repo
 from dal.queue_orgunit_repo import Queue_orgunit_repo
 from model.queue_orgunit import Queue_orgunit
 from model.orgunit import Orgunit
+from service.email_service import Email_service
 
 class Orgunit_queue_service:
     def __init__(self, constr_lora):
         self.orgunit_repo = Orgunit_repo(constr_lora)
         self.queue_repo = Queue_orgunit_repo(constr_lora)
 
-    def run_orgunit_queue_setup(self):
-        updated_orgs = self.orgunit_repo.get_orgunits('WHERE [updated] = 1')
-        deleted_orgs = self.orgunit_repo.get_orgunits('WHERE [deleted] = 1')
+    def run_orgunit_queue_setup(self, email_service):
+        orgs_to_handle = self.orgunit_repo.get_orgunits('WHERE [updated] = 1 or [deleted] = 1')
         orgs_to_update = {}
-        i=0
-        queue_orgs = {}
-        for los_id in updated_orgs:
-            # hvis org både er updated og deleted:
-            if los_id in deleted_orgs:
-                continue
-            else:
-                org = updated_orgs[los_id]
-                if org.uuid != None:
-                    que_org = Queue_orgunit(org.uuid, org.los_id, 'Updated')
-                    queue_orgs[i] = que_org
-                    i+=1
+        queue = {}
+        i = 0
+        for los_id in orgs_to_handle:
+            org = orgs_to_handle[los_id]
+            if org.updated == True and org.deleted == False:
+                queue_item = Queue_orgunit(org.uuid, org.los_id, 'Updated', True)
+                queue[i] = queue_item
+                i+=1
                 org.updated = False
-                orgs_to_update[org.los_id] = org
-        for los_id in deleted_orgs:
-            org = deleted_orgs[los_id]
-            que_org = Queue_orgunit(org.uuid, org.los_id, 'Deleted')
-            queue_orgs[i] = que_org
-            i+=1
-            org.deleted = False
+                orgs_to_update[los_id] = org
+            elif org.updated == False and org.deleted == True:
+                queue_item = Queue_orgunit(org.uuid, org.los_id, 'Deleted', True)
+                queue[i] = queue_item
+                i+=1
+                org.deleted = False
+                orgs_to_update[los_id] = org
+            else:
+                email_service.send_mail('jacob.aagaard.bennike@skanderborg.dk', 'SOFD Error: orgunit_queue.py', 'Orgunit er både opdateret og slettet los_id: ' + los_id)
+                continue
         self.orgunit_repo.update_orgunits(orgs_to_update)
-        self.queue_repo.insert_queue_orgunits(queue_orgs)
+        self.queue_repo.update_queue_orgunits(queue)
         
     def clean_orgunit_queue(self):
         queue_items_to_delete = self.queue_repo.get_orgunit_queueitems('WHERE [sts_org] = 1')
