f14a420c167f85e85f4d31963c967cba52146019
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-01-24 14:46:46 +0100

    deleted old code

--- SOFD/model/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, opus_id, uuid, samaccount, email, phone, mobile):
-        self.opus_id = opus_id
-        self.uuid = uuid
-        self.samaccount = samaccount
-        self.email = email
-        self.phone = phone
-        self.mobile = mobile
-        
