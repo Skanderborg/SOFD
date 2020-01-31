e658f530ac8940dcadaa336c263573ba8d591f91
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-01-31 09:36:28 +0100

    mox client for os2rollekatalog added to prod

--- Source/mox_os2rollekatalog_sync.py
+++ Source/mox_os2rollekatalog_sync.py
@@ -3,13 +3,30 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import glob
 from mox_clients.os2rollekatalog.os2rollekatalog_sync_service import Os2rollekatalog_sync_service
+from service.email_service import Email_service
+
+'''
+Mox klient som synkroniserer org data med OS2Rollekataloget.
+'''
+# henter variable fra .env filen
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 constr_lora = os.environ.get('constr_lora')
 api_key = os.environ.get('os2rollekatalog_apikey')
 endpointurl = os.environ.get('os2rollekatalog_endpointurl')
+error_email = os.environ.get('error_email')
 
+# email service
+es = Email_service(os.environ.get('smtp_username'), os.environ.get(
+    'smtp_password'), os.environ.get('smtp_server'), os.environ.get('smtp_port'))
 
-oss = Os2rollekatalog_sync_service(constr_lora)
-json_str = oss.create_org_json()
-#oss.post_json(endpointurl, api_key, json_str)
+# kører service
+try:
+    oss = Os2rollekatalog_sync_service(constr_lora)
+    step = 'Json'
+    json_str = oss.create_org_json()
+    step = 'api'
+    oss.post_json(endpointurl, api_key, json_str)
+except:
+    es.send_mail(error_email,
+                 'Error: mox_os2rollekatalog_sync.py python app', 'Error! ' + step)
