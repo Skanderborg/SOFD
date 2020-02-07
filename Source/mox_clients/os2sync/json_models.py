09222609cbeb94e768ef1422b6435c281c4f33f2
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-02-07 13:17:33 +0100

    orgunit jsonmodel for os2sync new api made

--- Source/mox_clients/os2sync/json_models.py
+++ Source/mox_clients/os2sync/json_models.py
@@ -2,21 +2,57 @@ import json
 
 
 class Orgunit_json:
-    def __init__(self, Uuid, ShortKey, Name, ParentOrgUnitUuid, Timestamp, Phone, Email):
+    def __init__(self, Uuid, ShortKey, Name, ParentOrgUnitUuid, PhoneNumber, Email, PayoutUnitUuid=None,
+                    Location=None, LOSShortName=None, ContactOpenHours=None, PhoneOpenHours=None, PostReturn=None,
+                    EmailRemarks=None, Contact=None, Ean=None, Post=None, Url=None, Landline=None):
         self.Uuid = Uuid
         self.ShortKey = ShortKey
         self.Name = Name
         self.ParentOrgUnitUuid = ParentOrgUnitUuid
-        self.Timestamp = Timestamp
-        self.Phone = Phone
+        self.PayoutUnitUuid = PayoutUnitUuid
+        self.PhoneNumber = PhoneNumber
         self.Email = Email
-        self.ItSystemUuids = []
+        self.Location = Location
+        self.LOSShortName = LOSShortName
+        self.ContactOpenHours = ContactOpenHours
+        self.PhoneOpenHours = PhoneOpenHours
+        self.PostReturn = PostReturn
+        self.EmailRemarks = EmailRemarks
+        self.Contact = Contact
+        self.Ean = Ean
+        self.Post = Post
+        self.Url = Url
+        self.Landline = Landline
+        self.Tasks = []
+        self.ContactForTasks = []
 
-    def add_ItSystemUuids(self, ItSystemUuid):
-        self.ItSystemUuids.append(ItSystemUuid)
+    def add_Task(self, task):
+        self.Tasks.append(task)
+
+    def add_ContractForTasks(self, contract):
+        self.ContactForTasks.append(contract)
 
     def reprJSON(self):
-        return dict(Uuid=self.Uuid, ShortKey=self.ShortKey, Name=self.Name, ParentOrgUnitUuid=self.ParentOrgUnitUuid, Timestamp=self.Timestamp, Phone=self.Phone, Email=self.Email, ItSystemUuids=self.ItSystemUuids)
+        return dict(Uuid=self.Uuid,
+                    ShortKey=self.ShortKey,
+                    Name=self.Name,
+                    ParentOrgUnitUuid=self.ParentOrgUnitUuid,
+                    PayoutUnitUuid=self.PayoutUnitUuid,
+                    PhoneNumber=self.PhoneNumber,
+                    Email=self.Email,
+                    Location=self.Location,
+                    LOSShortName=self.LOSShortName,
+                    ContactOpenHours=self.ContactOpenHours,
+                    PhoneOpenHours=self.PhoneOpenHours,
+                    PostReturn=self.PostReturn,
+                    EmailRemarks=self.EmailRemarks,
+                    Contact=self.Contact,
+                    Ean=self.Ean,
+                    Post=self.Post,
+                    Url=self.Url,
+                    Landline=self.Landline,
+                    Tasks=self.Tasks,
+                    ContactForTasks=self.ContactForTasks)
 
 
 class Generic_address_json:
---
+++ Source/test_os2sync.py
@@ -0,0 +1,10 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+# Vi har vores hemmelige værdier i en .env fil, hvis du skal bruge scriptet skal du have styr på disse.
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+# starter vores e-mail service - henter smtp informationer fra .env fil
+constr_lora = os.environ.get('constr_lora')
+
