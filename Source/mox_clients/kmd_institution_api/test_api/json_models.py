b444cf1dd2977e44a3c5800fa66c4105f9b6498d
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-06-03 11:22:19 +0200

    kmd institution mox client initiated - json models done

---
+++ Source/mox_clients/kmd_institution_api/test_api/json_models.py
@@ -0,0 +1,21 @@
+import json
+
+class employee:
+    def __init__(self, ssn, aliasName, email, endDate, startDate, transferToUserAdministration, mobilePhone, workPhone):
+        self.ssn = ssn
+        self.aliasName = aliasName
+        self.email = email
+        self.endDate = endDate
+        self.startDate = startDate
+        self.transferToUserAdministration = transferToUserAdministration
+        self.roles = []
+        self.mobilePhone = mobilePhone
+        self.workPhone = workPhone
+
+        def add_role(self, role):
+            self.roles.append(role)
+
+    def reprJSON(self):
+        return dict(ssn=self.ssn, aliasName=self.aliasName, email=self.email, endDate=self.endDate,
+                    startDate=self.startDate, transferToUserAdministration=self.transferToUserAdministration,
+                    roles=self.roles, mobilePhone=self.mobilePhone, workPhone = self.workPhone)
