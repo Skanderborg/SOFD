ad48aff34518045f224af700be8c5b21b06ba63d
Jacob Ågård Bennike <jacob@regnekraft.dk>
2020-08-20 16:13:44 +0200

    kmdrepo expanded

--- Source/mox_clients/kmd_institution_api/kmdl2_repo.py
+++ Source/mox_clients/kmd_institution_api/kmdl2_repo.py
@@ -11,7 +11,27 @@ class kmdl2_repo:
             cursor = cnxn.cursor()
             cursor.execute(
                 "SELECT [opus_id], \
-                        [los_id];")
+                        [los_id] \
+                FROM [LORA_SOFD].[kmdl2].[employees_in_tree];")
             for row in cursor.fetchall():
                 result[row.opus_id] = row.los_id
         return result
+
+'''
+SELECT [system_id]
+      ,[los_id]
+  FROM [LORA_SOFD].[kmdl2].[dagtilbud]
+
+
+SELECT [system_id]
+      ,[los_id]
+      ,[kmdl2_id]
+      ,[kmdl2_name]
+      ,[longname]
+  FROM [LORA_SOFD].[kmdl2].[institution_ids]
+
+SELECT [los_id]
+      ,[longname]
+      ,[parent_orgunit_los_id]
+  FROM [LORA_SOFD].[kmdl2].[institution_tree]
+'''
