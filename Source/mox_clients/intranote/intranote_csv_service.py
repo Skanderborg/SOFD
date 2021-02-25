69780b6f7411b928f9fd661ea7c8b22c13196eea
Jacob Ågård Bennike <jacob@regnekraft.dk>
2021-02-25 10:59:42 +0100

    intranote cvs logic exhumed from business logic, both necessary and well, best practice

--- Source/mox_clients/intranote/intranote_csv_service.py
+++ Source/mox_clients/intranote/intranote_csv_service.py
@@ -36,6 +36,43 @@ class Intranote_csv_service:
         usrs = usr_repo.get_users()
         pers = per_repo.get_persons()
         los_ids_to_ignore = pos_repo.get_disabled_orgunits()
+
+        # OBS!!! det her skal være klogere
+        tmp_cpr = []
+
+        cvs_ready_positions = []
+        for opus_id in poss:
+            pos = poss[opus_id]
+            if pos.los_id in los_ids_to_ignore:
+                continue
+
+            per = pers[pos.person_ref]
+            usr_userid = None
+            usr_email = None
+            usr_phone = None
+            usr_workmobile = None
+            if pos.uuid_userref != None:
+                usr = usrs[opus_id]
+                usr_userid = usr.userid
+                usr_email = usr.email
+                usr_phone = usr.phone
+                usr_workmobile = usr.workmobile
+        
+            cvs_ready_positions.append([pos.person_ref, per.firstname, per.lastname, opus_id, pos.uuid_userref, pos.los_id, pos.position_title,
+                                    pos.is_manager, pos.start_date, pos.leave_date, pos.manager_opus_id, pos.uuid_userref, usr_userid,
+                                    usr_email, usr_phone, usr_workmobile])
+
+            with open(csv_file_path + 'Medarbejder.csv', 'w', newline='', encoding='iso-8859-1') as file:
+                writer = csv.writer(file, delimiter=";")
+                writer.writerow(['cpr','firstname','lastname','opus_id','uuid_userref','los_id','title','is_manager','start_date',
+                                'leave_date','manager_opus_id','Uuid','UserId','Email','Phone','WorkMobile'])
+                for position in cvs_ready_positions:
+                    writer.writerow(position)
+
+
+
+        '''
+
         with open(csv_file_path + 'Medarbejder.csv', 'w', newline='', encoding='iso-8859-1') as file:
             writer = csv.writer(file, delimiter=";")
             writer.writerow(['cpr','firstname','lastname','opus_id','uuid_userref','los_id','title','is_manager','start_date',
@@ -56,11 +93,13 @@ class Intranote_csv_service:
                     usr_email = usr.email
                     usr_phone = usr.phone
                     usr_workmobile = usr.workmobile
+
+            
                 
                 writer.writerow([pos.person_ref, per.firstname, per.lastname, opus_id, pos.uuid_userref, pos.los_id, pos.position_title,
                                     pos.is_manager, pos.start_date, pos.leave_date, pos.manager_opus_id, pos.uuid_userref, usr_userid,
                                     usr_email, usr_phone, usr_workmobile])
-
+            '''
     def create_unic_csv(self, csv_file_path):
         unic_repo = Unic_username_repo(self.constr_lora)
         unics = unic_repo.get_unic_usernames('WHERE [opus_id] is not null')
