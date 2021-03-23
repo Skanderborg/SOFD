799a9393891ce11dabd2fe7a8b267fee274fb61e
Jacob Ågård Bennike <jacob@regnekraft.dk>
2021-03-23 09:57:47 +0100

    safetynet sql view now with actual code... dbo_v_it_license view also added and 8.3 numerator removed according to new licensing terms

---
+++ SQL/views/dbo_v_it_licens.sql
@@ -0,0 +1,26 @@
+USE [LORA_SOFD]
+GO
+
+/****** Object:  View [dbo].[v_it_licens]    Script Date: 23-03-2021 09:39:53 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+create view [dbo].[v_it_licens] as
+WITH orgs AS (SELECT        los_id, parent_orgunit_los_id, longName, niveau, ean
+                                FROM            pyt.orgunits
+                                WHERE        (parent_orgunit_los_id IN (847702, 834110, 878771, 878778, 878773, 878777, 878774, 878776, 878772, 859560, 871775, 874806, 834108, 847703, 851299, 846682)) OR
+                                                          (los_id IN (847702, 834110, 878771, 878778, 878773, 878777, 878774, 878776, 878772, 859560, 871775, 874806, 834108, 847703, 851299, 846682))
+                                UNION ALL
+                                SELECT        c.los_id, c.parent_orgunit_los_id, c.longName, c.niveau, c.ean
+                                FROM            pyt.orgunits AS c INNER JOIN
+                                                         orgs AS p ON c.parent_orgunit_los_id = p.los_id)
+    SELECT DISTINCT 
+                              mm.Fornavn, mm.Efternavn, mm.StillingBetegnelse, mm.ArbejdsstedNavn, mm.LOSOrganisationsenhed, mm.NearmesteLederADBrugerID, mm.[start_date], mm.Fratraad_dato, mm.OPUSMedarbejderExtraCiffer, 
+                              mm.numerator, mm.Omraede, mm.OmraedeID, mm.ErLeder, orgs_1.ean
+     FROM            orgs AS orgs_1 INNER JOIN
+                              dbo.v_sofd_medarbejderoverblik AS mm ON mm.LOSOrganisationID = orgs_1.los_id
+	-- WHERE        (mm.numerator > 8.3) gammel måde at måle på
+GO
---
+++ SQL/views/safetynet_v_afdelingsfil.sql
@@ -0,0 +1,29 @@
+USE [LORA_SOFD]
+GO
+
+/****** Object:  View [safetynet].[v_afdelingsfil]    Script Date: 23-03-2021 09:42:37 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+create view [safetynet].[v_afdelingsfil] as
+
+WITH orgs AS (SELECT        los_id, parent_orgunit_los_id, longName, niveau
+                                FROM            pyt.orgunits
+                                WHERE        (los_id IN (822041))
+                                UNION ALL
+                                SELECT        c.los_id, c.parent_orgunit_los_id, c.longName, c.niveau
+                                FROM            pyt.orgunits AS c INNER JOIN
+                                                         orgs AS p ON c.parent_orgunit_los_id = p.los_id)
+    SELECT        OrgUnit_1.los_id AS Afdelingskode, OrgUnit_1.longName AS Afdelingsnavn, OrgUnit_1.parent_orgunit_los_id AS Overafdelingskode, OrgUnit_1.pNr AS [VirksomhedsP.NR],
+                                  (SELECT        longName
+                                    FROM            pyt.Orgunits AS skborg
+                                    WHERE        (parent_orgunit_los_id = 0)) AS Virksomhedsnavn
+     FROM            orgs AS orgs_1 INNER JOIN
+                              pyt.orgunits AS OrgUnit_1 ON OrgUnit_1.los_id = orgs_1.los_id
+     WHERE        (OrgUnit_1.longName NOT LIKE '%#%')
+GO
+
+
