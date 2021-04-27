13d919cb898ff1e310aae0c18961eebe8a428c84
Jacob Ågård Bennike <jacob@regnekraft.dk>
2021-04-27 13:28:19 +0200

    dataflow documentation

--- diagrams/sofd_diagram.py
+++ diagrams/sofd_diagram.py
@@ -1,37 +1,50 @@
 from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS, EKS, Lambda
-from diagrams.aws.database import Redshift
+from diagrams.azure.compute import ContainerInstances, CloudServices, CloudServicesClassic
+from diagrams.azure.devops import Artifacts, Boards
+from diagrams.azure.database import SQLDatabases
 from diagrams.aws.integration import SQS
-from diagrams.aws.storage import S3
+from diagrams.aws.database import Redshift
+from diagrams.azure.identity import ActiveDirectory
 
-with Diagram('SOFD flow', show=False, direction='LR'):
+with Diagram('SOFD flow', show=False, direction='TB'):
 
     with Cluster('Data kilder'):
-        opus_data = EKS('Opus data')
-        uni_data = EKS('WS17')
-        ad_data = EKS('AD')
+        opus_data = Boards('Opus data')
+        uni_data = Artifacts('WS17')
+        ad_data = ActiveDirectory('AD')
 
-    azure_services = EKS('Azure_services')
+    azure_services = ContainerInstances('Azure_services')
 
 
     with Cluster('SOFD'):
         with Cluster('Services'):
-            opus_til_sofd = ECS('opus_til_sofd')
-            sofd_setup = ECS('sofd_setup')
-        sofden = S3('SQL')
-        sofden - ECS('logs')
+            opus_til_sofd = CloudServices('opus_til_sofd')
+            sofd_setup = CloudServices('sofd_setup')
+        sofden = SQLDatabases('LORA_SOFD')
+        sofden - Redshift('logs')
         queue = SQS("Ændrings kø")
         with Cluster('MOX agenter'):
-            os2_sync = ECS("OS2 Sync")
-            os2_rolle = ECS('Sofd til rolle')
+            mox_os2_sync = CloudServicesClassic("mox_os2sync")
+            mox_os2_rolle = CloudServicesClassic('mox_os2rollekatalog_sync')
+            mox_kmdi2 = CloudServicesClassic('mox_kmdi2_sync')
+            mox_intranote = CloudServicesClassic('mox_intranote')
+            mox_kalenda_greenbyte = CloudServicesClassic('mox_kalenda_greenbyte')
             
-    sts_org = S3('STS Organisation')
-    os2_rollekatalog = S3('OS2 Rollekatalog')
+    sts_org = Artifacts('STS Organisation')
+    intranettet = Artifacts('Intranettet')
+    os2_rollekatalog = Artifacts('OS2 Rollekatalog')
+    kmd_i2 = Artifacts('KMD i2 azure API')
+    aula = Artifacts('AULA')
+    kalenda = Artifacts('Kalenda')
+    kalenda - Artifacts('OPUS løn og personale')
 
 
     #kø og mox
-    queue >> os2_sync >> sts_org
-    sofden >> os2_rolle >> os2_rollekatalog
+    sofden >> mox_kalenda_greenbyte >> kalenda
+    sofden >> mox_intranote >> intranettet
+    sofden >> mox_kmdi2 >> kmd_i2 >> aula
+    queue >> mox_os2_sync >> sts_org
+    sofden >> mox_os2_rolle >> os2_rollekatalog
     #setup
     sofden >> sofd_setup >> queue << sofden
     uni_data >> sofd_setup >> sofden
