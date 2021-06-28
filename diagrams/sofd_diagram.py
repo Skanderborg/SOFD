67e1e6bf1126c06bee8db47a09f40871122e2943
Jacob Ågård Bennike <jacob@regnekraft.dk>
2021-06-28 14:54:58 +0200

    documentation updated

--- diagrams/sofd_diagram.py
+++ diagrams/sofd_diagram.py
@@ -5,12 +5,18 @@ from diagrams.azure.database import SQLDatabases
 from diagrams.aws.integration import SQS
 from diagrams.aws.database import Redshift
 from diagrams.azure.identity import ActiveDirectory
+from diagrams.azure.mobile import MobileEngagement
+from diagrams.azure.storage import DataBoxEdgeDataBoxGateway
+from diagrams.azure.web import APIConnections
+
+from diagrams.azure.compute import VMScaleSet, VMClassic
+
 
 with Diagram('SOFD flow', show=False, direction='TB'):
 
     with Cluster('Data kilder'):
         opus_data = Boards('KMD OPUS data')
-        uni_data = Artifacts('STIL unilogin WS17')
+        uni_data = DataBoxEdgeDataBoxGateway('STIL unilogin WS17')
         ad_data = ActiveDirectory('Skanderborg AD')
 
     azure_services = ContainerInstances('Skanderborg Azure Automation')
@@ -33,17 +39,18 @@ with Diagram('SOFD flow', show=False, direction='TB'):
             mox_acubiz = CloudServicesClassic('mox_acubiz')
             
     sts_org = Artifacts('STS Organisation')
-    sts_org - Artifacts('KOMBIT Systemer')
-    intranettet = Artifacts('Intranettet')
-    os2_rollekatalog = Artifacts('OS2 Rollekatalog')
-    kmd_i2 = Artifacts('KMD i2 azure API')
-    aula = Artifacts('AULA')
-    kalenda = Artifacts('Kalenda')
-    kalenda - Artifacts('OPUS løn og personale')
-    safetynet = Artifacts('Safetynet')
-    acubiz = Artifacts('Acubiz')
-    acubiz - Artifacts('Acubiz mobil App')
-    kombit_context_handler = Artifacts('KOMBIT Contaxhandler')
+    sts_org - VMScaleSet('KOMBIT Systemer')
+    intranettet = VMClassic('Intranettet')
+    os2_rollekatalog = VMClassic('OS2 Rollekatalog')
+    kmd_i2 = DataBoxEdgeDataBoxGateway('KMD i2 azure API')
+    aula = VMClassic('AULA')
+    kalenda = VMClassic('Kalenda')
+    kalenda - VMClassic('OPUS løn og personale')
+    safetynet = VMClassic('Safetynet')
+    acubiz = VMClassic('Acubiz')
+    acubiz - MobileEngagement('Acubiz mobil App')
+    kombit_context_handler = APIConnections('KOMBIT Contaxhandler')
+    os2sync = DataBoxEdgeDataBoxGateway('OS2Sync')
     #sbsys = Artifacts('SBSYS')
     #sbsys - Artifacts('SBSIP')
 
@@ -57,7 +64,7 @@ with Diagram('SOFD flow', show=False, direction='TB'):
     sofden >> mox_kalenda_greenbyte >> kalenda
     sofden >> mox_intranote >> intranettet
     sofden >> mox_kmdi2 >> kmd_i2 >> aula
-    queue >> mox_os2_sync >> sts_org
+    queue >> mox_os2_sync >> os2sync >> sts_org
     sofden >> mox_os2_rolle >> os2_rollekatalog >> kombit_context_handler
     #setup
     sofden >> sofd_setup >> queue << sofden
