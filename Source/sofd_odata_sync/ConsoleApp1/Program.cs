f96bf4d823dc73440b37ddb17646fe59f2fe459e
Jacob Ågård Bennike <jacob@regnekraft.dk>
2021-09-22 14:30:18 +0200

    odata sync

---
+++ Source/sofd_odata_sync/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Simple.OData.Client;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Uri u = new Uri("https://packages.nuget.org/v1/FeedService.svc");
+            var client = new ODataClient(u);
+            var packages = await client.FindEntriesAsync("Packages?$filter=Title eq 'Simple.OData.Client'");
+            foreach (var package in packages)
+            {
+                Console.WriteLine(package["Title"]);
+            }
+        }
+    }
+}
